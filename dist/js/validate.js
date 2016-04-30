c28ab94c438def1ff27aa6dd5d47bebc4bee4d41
Silvia J Boo <sljboo@gmail.com>
2016-04-30 18:26:10 +0200

    added descriptions of all projects

---
+++ dist/js/validate.js
@@ -0,0 +1,56 @@
+//scripts con funciones para validar formulario antes de enviar
+
+function validateEmail(sEmail) {
+    var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (filter.test(sEmail))
+        return true;
+    else
+        return false;
+};
+
+$('form').submit(function(ev){
+    ev.preventDefault();
+    var nombre=$('input[name="name"]').val();
+    var error = false;
+    if (nombre.length <3 || nombre.length >20){ //incorrecto
+        error = true;
+        $('input[name="name"]').next().show();
+    }
+    else{
+        $('input[name="name"]').next().hide();
+    }
+
+    var correo=$('input[name="email"]').val();
+    if (!validateEmail(correo)){ //is false
+        error = true;
+        $('input[name="email"]').next().show();
+    }
+    else{
+        $('input[name="email"]').next().hide();
+    }
+
+    ev.preventDefault();
+    var asunto=$('input[name="subject"]').val();
+    if (asunto.length <4 || asunto.length >50){ //incorrecto
+        error = true;
+        $('input[name="subject"]').next().show();
+    }
+    else{
+        $('input[name="subject"]').next().hide();
+    }
+
+    ev.preventDefault();
+    var msg=$('textarea[name="user_msg"]').val();
+    if (msg.length <10 || msg.length >200){ //incorrecto
+        error = true;
+        $('textarea[name="user_msg"]').next().show();
+    }
+    else{
+        $('textarea[name="user_msg"]').next().hide();
+    }
+
+    if (error==false)
+    {
+        $(this).submit();
+    }
+});
---
+++ src/php/sendEmail.php
@@ -0,0 +1,36 @@
+<?php
+if(isset($_POST['submit'])) {
+    $ver = floatval(phpversion());
+
+    $name = HTMLSPECIALCHARS($_POST['name']);
+    $msg = HTMLSPECIALCHARS($_POST['user_msg']);
+    $FromEmail = HTMLSPECIALCHARS($_POST['email']);
+	  $subject = HTMLSPECIALCHARS($_POST['subject']);
+    
+    $headers = '';
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $headers .="From: $From <$FromEmail>\n";
+    $headers .="Reply-To: <$FromEmail>\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .="Return-Path: <$FromEmail> \n";
+    $headers .="X-Sender: <$FromEmail>\n";
+    $headers .="X-Mailer: PHP-$ver \n";
+    $headers .="X-Priority: 3\n"; //1 UrgentMessage, 3 Normal    
+
+    $message = '<html> 
+       <head> 
+          <title>Message</title> 
+       </head>
+       <body>
+          <p><strong>Full name:</strong> ' . $name . '</p>
+          <p><strong>E-mail:</strong> ' . $FromEmail . '</p>
+		  <p><strong>Subject:</strong> ' . $subject . '</p>
+          <p><strong>Message:</strong> ' . $msg . '</p>
+       </body>
+       </html>';
+    if (mail('sljboo@gmail.com', 'Private Message', $message, $headers))
+        echo 'success';
+    else
+        echo 'fail';
+}
+?>
